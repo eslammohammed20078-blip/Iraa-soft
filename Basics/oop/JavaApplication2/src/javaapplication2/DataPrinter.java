b4412f4c36f88f0cc35dde800e33b0ee19516efe
eslammohammed20078-blip <eslammohammed20078@gmail.com>
2026-02-19 15:23:51 +0200

    Task lec3

---
+++ Basics/oop/JavaApplication2/src/javaapplication2/DataPrinter.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Eslam
+ */
+public class DataPrinter {
+public int a=3;
+public long x=1234567896;
+public char y='a';
+public float f=33.6f;
+public double z=14049.30493;
+void print(){
+    System.out.println(a);
+        System.out.println(x);
+    System.out.println(y);
+    System.out.println(f);
+        System.out.println(z);
+
+
+}
+    
+
+}
